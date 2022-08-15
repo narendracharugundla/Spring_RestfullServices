eb309980bd41baead690deac4159f8c4e27d58d1
Narendra Kumar Charugundla <naren.kumar426@gmail.com>
2022-08-15 16:52:23 +0530

    initial commit

---
+++ Spring-Boot-RestTemplate-main/spring-demo-webapp/src/main/java/com/cdackolkata/app/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.cdackolkata.app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cdackolkata.app.model.User;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Long> {
+
+}
---
+++ Spring-Boot-RestTemplate-main/spring-resttemplate-client-app/src/test/java/com/cdackolkata/app/SpringResttemplateClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdackolkata.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResttemplateClientAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
